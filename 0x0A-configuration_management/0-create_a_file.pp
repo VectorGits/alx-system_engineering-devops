dd5ac588f61ad8599c7c6f82c6e766b4b4987e10
VectorGits <davidoluremi0@gmail.com>
2024-04-20 02:20:45 +0100

    Fixed linting err

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # This script creates a file in the /tmp/ folder
 file { '/tmp/school':
-	ensure  => 'file',
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => 'I love Puppet',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
