4e220eb7be820054185eae67d13fd8abdb291261
VectorGits <davidoluremi0@gmail.com>
2024-04-21 18:46:56 +0100

    Refactor script to use exec resource

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,21 +1,5 @@
-# Client config file w/ puppet
+# Client configuration file (w/Puppet)
 
-file { '/home/ubuntu/.ssh':
-  ensure => 'directory',
-  mode   => '0700',
-}
-
-file { '/home/ubuntu/.ssh/config':
-  ensure => 'present',
-  mode   => '0600',
-}
-
-file_line { 'Declare identity file':
-  path => '/home/ubuntu/.ssh/config',
-  line => 'IdentityFile ~/.ssh/school',
-}
-
-file_line { 'Turn off passwd auth':
-  path => '/home/ubuntu/.ssh/config',
-  line => 'PasswordAuthentication no',
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+  path    => '/bin/'
 }
