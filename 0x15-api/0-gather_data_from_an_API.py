513b9b332d199e7d0ca81a0ece0fa12d40b3c33d
VectorGits <davidoluremi0@gmail.com>
2024-05-31 18:15:42 +0100

    chore: Refactor script to gather and display employee TODO list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
 '''
-A Python script that, using this REST API,
-for a given employee ID, returns information
-about his/her TODO list progress.
+gather employee data from API
 '''
 
+import re
 import requests
 import sys
-# import re
 
+REST_API = "https://jsonplaceholder.typicode.com"
 
-def get_employee_todo_progress(employee_id):
-    """Fetches and prints the todo list progress of an employee by id."""
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-
-    user = requests.get(user_url).json()['name']
-    todo_list = requests.get(todo_url).json()
-
-    total_tasks = len(todo_list)
-    done_tasks = len([task for task in todo_list if task['completed']])
-    done_tasks_titles = [task['title'] for task in todo_list if task['completed']]
-
-    print(f"Employee {user} is done with tasks({done_tasks}/{total_tasks}):")
-    for title in done_tasks_titles:
-        print(f"\t {title}")
-
-    if __name__ == "__main__":
-        employee_id = sys.argv[1]
-        get_employee_todo_progress(employee_id)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            task_req = requests.get('{}/todos'.format(REST_API)).json()
+            emp_name = req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
+            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    emp_name,
+                    len(completed_tasks),
+                    len(tasks)
+                )
+            )
+            if len(completed_tasks) > 0:
+                for task in completed_tasks:
+                    print('\t {}'.format(task.get('title')))
