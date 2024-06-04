b61304311671494cbd4c3600396f7905c7a3505d
VectorGits <davidoluremi0@gmail.com>
2024-06-04 20:02:56 +0100

    chore: func to query reddit API and print titles of popular posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Function that queries the Reddit API and
+    prints the titles of the first 10
+    hot posts listed for a given subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    data = response.json()
+
+    posts = data.get('data', {}).get('children', [])
+    for post in posts:
+        print(post.get('data', {}).get('title'))
+
+    return
