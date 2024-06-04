c657d7ad12f0bda381f2f4485ed25067d6efed38
VectorGits <davidoluremi0@gmail.com>
2024-06-04 19:02:27 +0100

    chore: refactor code to pass pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """Function that queries the Reddit API and returns the number of subscribers
 """
-
 import requests
 
+
 def number_of_subscribers(subreddit):
-	"""Function that queries the Reddit API and returns the number of subscribers
-	"""
-	url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-	headers = {'User-Agent': 'Mozilla/5.0'}
+    """Function that queries the Reddit
+    API and returns the number of subscribers
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
-	response = requests.get(url, headers=headers, allow_redirects=False)
-	if response.status_code != 200:
-		return 0
-	data = response.json()
-	subscribers = data.get('data', {}).get('subscribers', 0)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    data = response.json()
+    subscribers = data.get('data', {}).get('subscribers', 0)
 
-	return subscribers
+    return subscribers
